bafc0ee38bac87f1a2895513084d62791ad4dc39
Lucien Gaitskell <lucieng@gaitskell.com>
2019-08-27 09:36:09 -0400

    Add setup file for pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='python-nmealite',
+    version='0.1',
+    description='A lightweight (Micro)Python NMEA en/decoder',
+    author='Lucien Gaitskell',
+    url='https://github.com/luciengaitskell/python-nmealite',
+    packages=find_packages()
+)
